8db49dbe157e4a46a7b7e26efee19c6c2fa1dab6
Rodolfo Labiapari Mansur Guimarães <rodolfolabiapari@gmail.com>
2017-04-03 09:24:05 -0300

    Sistema funcionando teoricamente com os carrinhos master e salve

--- ploudy.py
+++ ploudy.py
@@ -5,56 +5,47 @@ import function
 HOST = ""  # Symbolic name, meaning all available interfaces
 PORT = 8888  # Arbitrary non-privileged port
 
-ROBOT_SENSORS = "M"
-ROBOT_N_SENSORS = "S"
+ROBOT_SENSORS = "Master Robot"
+ROBOT_WITHOUT_SENSORS = "Slave Robot"
 
 
-# Address Family : AF_INET (this is IP version 4 or IPv4)
-# Type : SOCK_STREAM (this means connection oriented TCP protocol)
+def main():
+    # Address Family : AF_INET (this is IP version 4 or IPv4)
+    # Type : SOCK_STREAM (this means connection oriented TCP protocol)
 
-print "[INFO] Starting the Ploudy"
-ploudy_server = function.start_server(HOST, PORT)
+    function.print_debug("[INFO] Starting the Ploudy")
+    ploudy_server = function.start_server(HOST, PORT)
 
-while 1:
+    l_data_base = []
 
-    print "[INFO] Waiting a new client..."
+    while 1:
 
-    # wait to accept a connection - blocking call
-    # addr0 is the IP and and addr1 is the ports
-    client_connection, client_addr = ploudy_server.accept()
-    print "[INFO] Connected with " + client_addr[0] + " : " + str(client_addr[1])
+        function.print_debug("[INFO] Waiting a new client...")
 
-    robot = client_connection.recv(1024)
+        # wait to accept a connection - blocking call
+        # addr0 is the IP and and addr1 is the ports
+        client_connection, client_addr = ploudy_server.accept()
+        function.print_debug("\n[INFO] Connected with " + client_addr[0] + " : " + str(client_addr[1]))
 
-    if robot == ROBOT_SENSORS:
+        robot = client_connection.recv(1024)
 
-        print "[INFO] <led_esq> <led_dir>  <acel_x> <acel_y> <acel_z>  <giro_x> <giro_y> <giro_z>"
+        function.print_debug("\n\tReceived: \"" + robot + "\"")
 
-        # now keep talking with the client
-        while 1:
-
-            # now keep talking with the client
-            package = client_connection.recv(1024)
-            print "Received: \"", package, "\""
-
-            # Verify the parameters
-            led_esq, led_dir, acel_x, acel_y, acel_z, giro_x, giro_y, giro_z = function.verify_parameters(package)
-
-            # Process
-            motor_esq, motor_dir = function.calcule_new_action(led_esq, led_dir, acel_x, acel_y, acel_z, giro_x, giro_y, giro_z)
-
-
-            # Respond
-            reply = "Received: \"", package, "\""
-            if "exit" in package or\
-                            "finnish" in package:
-                break
-
-            client_connection.sendall(reply)
+        if ROBOT_SENSORS in robot:
+            if len(l_data_base) > 0:
+                function.print_debug("[ERRO] Client was desconnected!")
+            else:
+                try:
+                    function.sensors_robot_procedure(client_connection, l_data_base)
+                except:
+                    function.print_debug("[ERRO] Client was desconnected by a error!")
+        elif ROBOT_WITHOUT_SENSORS in robot:
+            try:
+                function.slave_robot_procedure(client_connection, l_data_base)
+            except:
+                function.print_debug("[ERRO] Client was desconnected by a error!")
 
         client_connection.close()
+    ploudy_server.close()
 
-        if "exit" in package:
-            break
-
-ploudy_server.close()
+main()
